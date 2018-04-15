bf5779180bf0e3cb6c967f87d6262a976e055e32
Daiderd Jordan <daiderd@gmail.com>
2018-04-15 19:14:31 +0200

    cleanup nix expression

--- default.nix
+++ default.nix
@@ -1,21 +1,39 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  inherit (pkgs) stdenv fetchFromGitHub writeText runCommand;
+  inherit (pkgs) stdenv fetchFromGitHub writeText runCommand vim;
 
-  vim-vader = fetchFromGitHub {
+  # Fallback for nix 1.11
+  fetchGit = builtins.fetchGit or (x: x);
+
+  vader = fetchFromGitHub {
     owner = "junegunn";
     repo = "vader.vim";
     rev = "ddb714246535e814ddd7c62b86ca07ffbec8a0af";
     sha256 = "0jlxbp883y84nal5p55fxg7a3wqh3zny9dhsvfjajrzvazmiz44n";
   };
+in
 
-  vim-nix = ./.;
+stdenv.mkDerivation rec {
+  name = "vim-nix-${version}${versionSuffix}";
+  version = "0.1.0";
+  versionSuffix = "pre${toString src.revCount or 0}.${src.shortRev or "0000000"}";
+  src = fetchGit ./.;
 
-  rcFile = writeText "vimrc" ''
+  dontBuild = true;
+  preferLocalBuild = true;
+
+  buildInputs = [ vim ];
+
+  installPhase = ''
+    mkdir -p $out
+    cp -r ftdetect ftplugin indent syntax $out
+  '';
+
+  vimrc = writeText "vimrc" ''
     filetype off
-    set rtp+=${vim-vader}
-    set rtp+=${vim-nix}
+    set rtp+=${vader}
+    set rtp+=${src}
     filetype plugin indent on
     syntax enable
 
@@ -26,32 +44,15 @@ let
     command! Syntax call Syntax()
   '';
 
-  vim = "${pkgs.vim}/bin/vim";
-
-  env = stdenv.mkDerivation {
-    name = "build-environment";
-    shellHook = ''
-      alias vim='${vim} -XNu ${rcFile} -i NONE'
-    '';
-  };
-
-in stdenv.mkDerivation {
-  name = "vim-nix-2017-04-30";
-  src = ./.;
-
-  passthru = { inherit env; };
-
-  dontBuild = true;
-  preferLocalBuild = true;
-
-  installPhase = ''
-    mkdir -p $out
-    cp -r ftdetect ftplugin indent syntax $out
-  '';
-
   checkPhase = ''
-    ( ${vim} -XNu ${rcFile} -i NONE -c 'Vader! test/*.vader' ) |& tee vim-nix-test.log >&2
+    ( vim -XNu ${vimrc} -i NONE -c 'Vader! test/*.vader' ) |& tee vim-nix-test.log >&2
   '';
 
   doCheck = true;
+
+  shellHook = ''
+    vim() {
+        command vim -XNu ${vimrc} -i NONE "$@"
+    }
+  '';
 }
